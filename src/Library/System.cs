cf10c8b1ffd72a36e7f74c6770ceb7930d00d209
Mauripache <mauri_pache@hotmail.com>
2020-10-07 21:13:43 -0300

    System class progress

--- src/Library/System.cs
+++ src/Library/System.cs
@@ -1,9 +1,11 @@
 using System.Collections.Generic;
+using TwitterUCU;
 
 namespace Library
 {
-    public class RideSystem
+    public class RideSystem 
     {
+        private TwitterImage twitterImage= new TwitterImage("CkovShLMNVCY0STsZlcRUFu99", "6rc35cHCyqFQSy4vIIjKiCYu31qqkBBkSS5BRlqeYCt5r7zO5B", "1396065818-MeBf8ybIXA3FpmldORfBMdmrVJLVgijAXJv3B18", "gNytQjJgLvurJekVU0wmBBkrR1Th40sJmTO8JDhiyUkuy");
         private List<Passenger> passengers;
         public List<Passenger> Passengers{get;}
         private List<Driver> drivers;
@@ -20,6 +22,7 @@ namespace Library
             if (!drivers.Contains(newDriver))
             {
                 drivers.Add(newDriver);
+                twitterImage.PublishToTwitter($"Welcome to Uber, I mean UCURide. Our new driver {newDriver.Name}.\n{newDriver.Bio}.", newDriver.Picture);
             }
         }
 
@@ -28,12 +31,8 @@ namespace Library
             if (!passengers.Contains(newPassenger))
             {
                 passengers.Add(newPassenger);
+                twitterImage.PublishToTwitter($"Welcome to Uber, I mean UCURide. Our new passenger {newPassenger.Name}.", newPassenger.Picture);
             }
-        }
-
-        private void SendMessage(Person person)
-        {
-            
         }
     }
 
